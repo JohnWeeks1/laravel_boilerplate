cdf0c7a6769bd13f35c26248ea90fd9ac5b5d41c
vagrant <vagrant@homestead>
2018-07-30 12:00:43 +0000

    Second Commit

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        JeroenNoten\LaravelAdminLte\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
